0ae99f7cfe47d907b926983d49425ed19257b873
Zakk Hoyt <zakk@threefoldphotos.com>
2014-02-21 11:08:17 -0800

    Initial Commit

---
+++ theremin/VWWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWViewController.h
+//  theremin
+//
+//  Created by Zakk Hoyt on 2/21/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWViewController : UIViewController
+
+@end
---
+++ thereminTests/thereminTests.m
@@ -0,0 +1,34 @@
+//
+//  thereminTests.m
+//  thereminTests
+//
+//  Created by Zakk Hoyt on 2/21/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface thereminTests : XCTestCase
+
+@end
+
+@implementation thereminTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
